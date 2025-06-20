237ff09bff392ecc5e93ac2ffff015be5ea574a3
Marcos Xavier Gil Júnior <marcosxaviergiljr@gmail.com>
2025-06-20 19:51:13 -0300

    update

--- apps/badgrsocialauth/providers/ufsc/__init__.py
+++
@@ -1,4 +0,0 @@
-# apps/badgrsocialauth/providers/ufsc/__init__.py
-# CORREÇÃO PERMANENTE
-
-default_app_config = 'badgrsocialauth.providers.ufsc.apps.UfscProviderConfig'
--- apps/badgrsocialauth/providers/ufsc/urls.py
+++
@@ -1,6 +0,0 @@
-# apps/badgrsocialauth/providers/ufsc/urls.py
-
-from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-from .provider import UfscProvider
-
-urlpatterns = default_urlpatterns(UfscProvider)
